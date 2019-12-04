2a2c5a609007c904324c12a3d86a802b6aa59f2d
Markel <markelrodriguez19@gmail.com>
2019-12-04 14:39:21 +0100

    index.css y cambio de imagenes de la vista

--- index.php
+++ index.php
@@ -1,106 +1,119 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Zornotza Quidditch</title>
-        
-        <!--<link rel="icon" type="image/png" href="view/img/favicon.ico"> -->
-        <link href="view/css/style.css" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">   
-    </head>
-    <body>
-    	<header>
-        	<!-- NAV -->
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-              <a class="navbar-brand" href="#">Zornotza Quidditch</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav mr-auto">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Equipos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Administración</a>
-                  </li>
-                </ul>
-                <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                  <div class="btn-group mr-2" role="group" aria-label="First group">
-                    <button type="button" class="btn btn-outline-light">Regístrate</button>
-                  </div>
-                  <div class="btn-group mr-2" role="group" aria-label="Second group">
-                    <button type="button" class="btn btn-light">Iniciar Sesión</button>
-                  </div>
-                </div>
-              </div>
-            </nav>
-            <!-- FIN NAV -->
-    	</header>
-        <main role="main">
-            <!-- INICIO JUMBOTRÓN -->
-            <section class="jumbotron jumbotron-fluid">
-              <div class="container">
-                <h1 class="display-4">¿QUIENES SOMOS?</h1>
-                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse corporis maxime inventore quam optio impedit ex mollitia temporibus labore porro voluptates fuga incidunt similique vero dicta minima id, officiis, saepe commodi ducimus modi est obcaecati! Nulla, tenetur soluta voluptatum laboriosam illo fuga iste quo? Quidem, fuga! Repellat amet, temporibus numquam unde beatae nostrum quibusdam hic quam porro aut iste dignissimos et ex in? Facere aspernatur rem tempora eos blanditiis atque aperiam mollitia quod, numquam quos ab recusandae possimus omnis corrupti iste sunt esse itaque molestiae aliquam deserunt distinctio! Eligendi eum deserunt molestiae aliquam. Soluta, aliquam. Quos a molestiae rem aperiam!</p>
-              </div>
-            </section>
-            <!-- FIN JUMBOTRÓN -->
-        
-          <div class="album py-5 bg-light">
-            <div class="container">
-        
-              <div class="row">
-                <div class="col-md-4">
-                  <div class="card mb-4 shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">9 mins</small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="card mb-4 shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">9 mins</small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="card mb-4 shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">9 mins</small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zornotza Quidditch</title>
+    
+    <script src="https://kit.fontawesome.com/661afcc94b.js"></script>
+    <!--<link rel="icon" type="image/png" href="view/img/favicon.ico"> -->
+    <link href="view/css/index.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+	<header>
+		<!-- NAV -->
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#"><i class="fas fa-quidditch"></i></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse"
+				data-target="#navbarText" aria-controls="navbarText"
+				aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarText">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item"><a class="nav-link" href="#">Equipos</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Administración</a></li>
+				</ul>
+				<div class="btn-toolbar" role="toolbar"
+					aria-label="Toolbar with button groups">
+					<div class="btn-group mr-2" role="group" aria-label="First group">
+						<button type="button" class="btn btn-outline-light">Regístrate</button>
+					</div>
+					<div class="btn-group mr-2" role="group" aria-label="Second group">
+						<button type="button" class="btn btn-light">Iniciar Sesión</button>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<!-- FIN NAV -->
+	</header>
+	<main> 
+    <!-- JUMBOTRÓN -->
+    <section class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">¿QUIENES SOMOS?</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Esse corporis maxime inventore quam optio impedit ex mollitia
+          temporibus labore porro voluptates fuga incidunt similique vero
+          dicta minima id, officiis, saepe commodi ducimus modi est obcaecati!
+          Nulla, tenetur soluta voluptatum laboriosam illo fuga iste quo?
+          Quidem, fuga! Repellat amet, temporibus numquam unde beatae nostrum
+          quibusdam hic quam porro aut iste dignissimos et ex in? Facere
+          aspernatur rem tempora eos blanditiis atque aperiam mollitia quod,
+          numquam quos ab recusandae possimus omnis corrupti iste sunt esse
+          itaque molestiae aliquam deserunt distinctio! Eligendi eum deserunt
+          molestiae aliquam. Soluta, aliquam. Quos a molestiae rem aperiam!</p>
+      </div>
+    </section>
+    <!-- FIN JUMBOTRÓN -->
+
+    <!-- CARDS -->
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 mb-4">
+            <div class="card mb-4 shadow-sm" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">¿Quiénes somos?</h5>
+                <p class="card-text">Some quick example text to build on the card
+                  title and make up the bulk of the card's content. Some quick example 
+                  text to build on the card title and make up the bulk of the card's content.</p>
               </div>
             </div>
           </div>
-        </main> 
-    </body>
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 mb-4">
+            <div class="card mb-4 shadow-sm" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">¿Qué es el Quidditch?</h5>
+                <p class="card-text">Some quick example text to build on the card
+                  title and make up the bulk of the card's content. Some quick example 
+                  text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 mb-4">
+            <div class="card mb-4 shadow-sm" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">¿Cómo se juega?</h5>
+                <p class="card-text">Some quick example text to build on the card
+                  title and make up the bulk of the card's content. Some quick example 
+                  text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- FIN CARDS -->
+	</main>
+	
+	<!-- FOOTER -->
+    <footer class="footer text-muted text-center bg-secondary">
+    	<ul class="list-inline pt-3">
+    		<li class="list-inline-item"><a href="https://es-es.facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="https://twitter.com/?lang=es"><i class="fab fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="https://www.instagram.com/?hl=es"><i class="fab fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="https://www.youtube.com/?gl=ES&hl=es"><i class="fab fa-youtube"></i></a></li>
+            <li class="list-inline-item"><a href="https://support.google.com/plus/?hl=es-419#topic=9259565"><i class="fab fa-google-plus-g"></i></a></li>
+        </ul>
+        <p class="mb-1">&copy; Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla maiores ipsam in inventore voluptatibus. Labore, provident. Quos ratione accusantium facere? Rem assumenda, a adipisci possimus repudiandae ipsam mollitia debitis architecto.</p>
+    </footer>
+    <!-- FIN FOOTER -->
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
 </html>
